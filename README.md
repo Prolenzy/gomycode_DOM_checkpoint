7de87adba110afb8237d58ef69ae35d5581d6f28
Charles Kimotho <tutornewman002@gmail.com>
2023-12-23 16:37:33 +0300

    Initial commit of the shopping cart project

---
+++ README.md
@@ -0,0 +1,14 @@
+Project title: A Shopping Cart
+
+
+(1) To increase the quantity of an item, click on the "+" button.
+
+(2) To decrease the quantity of an item, click on the "-" button.
+
+(3) The value "1" next to the quantity buttons (either the "+" or "-" buttons) represents the default quantity of the item in the shopping cart. When a user views the shopping cart, they see this default quantity value. If they wish to adjust the quantity, they can click the "+" or "-" buttons to increase or decrease the quantity respectively.
+
+(4) Each item row will have a delete option, usually represented by an 'X' icon. Clicking on this will remove the item from the cart.
+
+(5) Each item row will have a heart-shaped button. Clicking on this will toggle the liked state of the item. The heart will change color to indicate if the item is liked or not.
+
+(6) The total price will be dynamically adjusted based on the quantity of each item and any deletions made.
---
+++ script.js
@@ -0,0 +1,36 @@
+// Function to adjust the quantity of items
+function adjustQuantity(btn, action) {
+    let quantityElement = btn.parentElement.querySelector('span');
+    let currentQuantity = parseInt(quantityElement.textContent);
+    // Ensure quantity doesn't go below 1
+    if (action === '-' && currentQuantity > 1) {
+        currentQuantity--;
+    } else if (action === '+') {
+        currentQuantity++;
+    }
+    quantityElement.textContent = currentQuantity;
+    updateTotalPrice();
+}
+// Function to delete an item from the cart
+function deleteItem(btn) {
+    let row = btn.parentElement.parentElement;
+    row.remove();
+    updateTotalPrice();    
+}
+// Function to toggle the liked state of an item
+function toggleLike(btn) {
+    btn.classList.toggle('liked');
+}
+// Function to update the total price based on item quantities user selects
+function updateTotalPrice() {
+    let rows = document.querySelectorAll('#cartTable tbody tr');
+    let totalPrice = 0;
+
+    rows.forEach(row => {
+        let quantity = parseInt(row.querySelector('span').textContent);
+        let price = parseFloat(row.querySelector('td:nth-child(3)').textContent.substring(1));
+        totalPrice += quantity * price;
+    });
+    // Display the total price of the selected items
+    document.getElementById('totalPrice').textContent = '$' + totalPrice.toFixed(2);
+}
